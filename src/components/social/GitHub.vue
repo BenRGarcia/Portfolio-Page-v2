9f3051572225875b148045a9a571e30ec3949124
Ben Garcia <ben@seebenprogram.com>
2018-03-23 11:39:30 -0400

    Build social link component for github

---
+++ src/components/social/GitHub.vue
@@ -0,0 +1,27 @@
+<template>
+  <a
+    :href="href"
+    target="_blank"
+  >
+    <img
+      :src="imageUrl"
+      alt="Octocat"
+      class="img-fluid"
+    >
+  </a>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      imageUrl: require('../../assets/images/Octocat/Octocat.png'),
+      href: 'https://github.com/BenRGarcia'
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>

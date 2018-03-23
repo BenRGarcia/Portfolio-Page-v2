f813accd12efb2b45cf4c2976a0576fabca60e8a
Ben Garcia <ben@seebenprogram.com>
2018-03-23 12:30:23 -0400

    Build logo component

---
+++ src/components/logo/Logo.vue
@@ -0,0 +1,28 @@
+<template>
+  <div :class="logoClasses">
+    <h2 :class="headingClasses">Benjamin<br>Garcia</h2>
+    <span :class="subheadingClasses">Full Stack Developer</span>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      logoClasses: [
+        'p-5'
+      ],
+      headingClasses: [
+        'h1'
+      ],
+      subheadingClasses: [
+        'h6'
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>

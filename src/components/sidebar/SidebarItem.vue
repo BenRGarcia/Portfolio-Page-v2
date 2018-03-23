d1088033b9cbbd47d6382f29998c4c2e221efd66
Ben Garcia <ben@seebenprogram.com>
2018-03-22 22:20:27 -0400

    Add component, bind classes

---
+++ src/components/sidebar/SidebarItem.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <li
+      :class="itemClasses"
+    >
+      <a
+        :href="href"
+        :class="linkClasses"
+        v-html="name"
+      >
+      </a>
+    </li>
+  </div>
+</template>
+
+<script>
+export default {
+  props: [
+    'href', 'name'
+  ],
+  data () {
+    return {
+      itemClasses: [
+        'nav-item', 'pt-2', 'pb-0'
+      ],
+      linkClasses: [
+        'nav-link'
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>

ee906ff48b7e466fd92ec7959dddc46116821bdc
Ben Garcia <ben@seebenprogram.com>
2018-03-22 22:21:02 -0400

    Build component, bind sidebar to vuex store's state

---
+++ src/components/sidebar/Sidebar.vue
@@ -0,0 +1,42 @@
+<template>
+  <nav :class="sidebarClasses">
+    <ul :class="ulClasses">
+      <!-- Sidebar links -->
+      <SidebarItem
+        v-for="(link, index) in sidebarLinks"
+        :key="index"
+        :href="link.href"
+        :name="link.name"
+      />
+    </ul>
+  </nav>
+</template>
+
+<script>
+import SidebarItem from './SidebarItem'
+
+export default {
+  data () {
+    return {
+      sidebarClasses: [
+        'col-3', 'bg-light', 'pl-0', 'pr-0'
+      ],
+      ulClasses: [
+        'nav', 'nav-pills', 'flex-column'
+      ]
+    }
+  },
+  components: {
+    SidebarItem
+  },
+  computed: {
+    sidebarLinks () {
+      return this.$store.getters.getSidebarLinks
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>

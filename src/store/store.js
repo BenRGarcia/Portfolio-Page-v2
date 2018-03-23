bec8cb9d76ba487b4b9b69e474764ecf423b01fe
Ben Garcia <ben@seebenprogram.com>
2018-03-22 22:20:04 -0400

    Add sidebar link to state

--- src/store/store.js
+++ src/store/store.js
@@ -4,8 +4,31 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export const store = new Vuex.Store({
-  state: {},
-  getters: {},
+  state: {
+    _sidebarLinks: [
+      {
+        name: 'Home',
+        href: 'https://benrgarcia.github.io/Portfolio-Page-v2/#/'
+      },
+      {
+        name: 'About',
+        href: 'https://benrgarcia.github.io/Portfolio-Page-v2/#/about'
+      },
+      {
+        name: 'Portfolio',
+        href: 'https://benrgarcia.github.io/Portfolio-Page-v2/#/portfolio'
+      },
+      {
+        name: 'R&eacute;sum&eacute;',
+        href: 'https://benrgarcia.github.io/Portfolio-Page-v2/#/resume'
+      }
+    ]
+  },
+  getters: {
+    getSidebarLinks (state) {
+      return state._sidebarLinks
+    }
+  },
   mutations: {},
   actions: {}
 })

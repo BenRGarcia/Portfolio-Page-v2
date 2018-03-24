b2fc4119cf5f62f9c1bca03233bb56a31dfb5735
Ben Garcia <ben@seebenprogram.com>
2018-03-24 12:44:08 -0400

    Remove template's HelloWorld component path

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Home from '@/components/Home'
+import About from '@/components/About'
+import Portfolio from '@/components/Portfolio'
+import Resume from '@/components/Resume'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    },
     {
       path: '/home',
       name: 'Home',

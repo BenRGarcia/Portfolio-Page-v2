a9bd84ca0ef29743ae68ffd81f58effa92c0fdbc
Ben Garcia <ben@seebenprogram.com>
2018-03-24 12:33:55 -0400

    Define paths, wildcard redirect

--- src/router/index.js
+++ src/router/index.js
@@ -10,6 +10,30 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/home',
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/about',
+      name: 'About',
+      component: About
+    },
+    {
+      path: '/portfolio',
+      name: 'Portfolio',
+      component: Portfolio
+    },
+    {
+      path: '/resume',
+      name: 'Resume',
+      component: Resume
+    },
+    {
+      path: '*',
+      redirect: '/home'
     }
   ]
 })

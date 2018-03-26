828244bab705319fd8982358605d9b783c567778
Ben Garcia <ben@seebenprogram.com>
2018-03-26 14:45:24 -0400

    Add jumbotron component

--- src/components/jumbotron/Jumbotron.vue
+++ src/components/jumbotron/Jumbotron.vue
@@ -1,5 +1,12 @@
 <template>
-  
+  <div class="jumbotron jumbotron-fluid">
+    <div class="container">
+      <h1 class="display-4">Hi, I'm Ben</h1>
+      <hr class="my-4">
+      <p class="lead">Looking for a Full-Stack JavaScript Ninja?</p>
+      <p class="blockquote-footer">I'm your guy...</p>
+    </div>
+  </div>
 </template>
 
 <script>

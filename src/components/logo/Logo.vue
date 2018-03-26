f9dabfca4b3779d46e2b0e99748fc1372445b1df
Ben Garcia <ben@seebenprogram.com>
2018-03-26 12:29:10 -0400

    Add profile image to sidebar

--- src/components/logo/Logo.vue
+++ src/components/logo/Logo.vue
@@ -1,5 +1,6 @@
 <template>
   <div :class="logoClasses">
+    <img :src="imgUrl" :alt="imgAlt" :class="profileImageClasses">
     <h2 :class="headingClasses">Benjamin<br>Garcia</h2>
     <span :class="subheadingClasses">Full Stack Developer</span>
   </div>
@@ -10,19 +11,26 @@ export default {
   data () {
     return {
       logoClasses: [
-        'p-5'
+        'p-4'
+      ],
+      profileImageClasses: [
+        'img-fluid', 'rounded-circle', 'border', 'border-danger'
       ],
       headingClasses: [
-        'h1', 'mb-0'
+        'h1', 'mb-0', 'mt-3'
       ],
       subheadingClasses: [
         'h6'
-      ]
+      ],
+      imgUrl: require('../../assets/images/profile.jpeg'),
+      imgAlt: 'Profile picture of Benjamin Garcia, Full Stack Web Developer'
     }
   }
 }
 </script>
 
 <style lang="scss" scoped>
-
+.border {
+  border-width: 8px !important;
+}
 </style>

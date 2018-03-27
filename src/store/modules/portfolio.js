9a7d3d91db8379f816571026edb63af29a1d5d41
Ben Garcia <ben@seebenprogram.com>
2018-03-27 17:07:01 -0400

    Add initial dummy content

--- src/store/modules/portfolio.js
+++ src/store/modules/portfolio.js
@@ -1,6 +1,29 @@
-const state = {}
+const state = {
+  _projects: [
+    {
+      name: 'Liri',
+      description: 'A Node.js command line app that mimics Siri.',
+      imgUrl: '../../assets/images/portfolio/vue.png',
+      href: 'https://github.com/BenRGarcia/liri-node-app'
+    },
+    {
+      name: 'Miri',
+      description: 'A Node.js command line app that mimics Siri.',
+      imgUrl: '../../assets/images/portfolio/vue.png',
+      href: 'https://github.com/BenRGarcia/liri-node-app'
+    },
+    {
+      name: 'Niri',
+      description: 'A Node.js command line app that mimics Siri.',
+      imgUrl: '../../assets/images/portfolio/vue.png',
+      href: 'https://github.com/BenRGarcia/liri-node-app'
+    }
+  ]
+}
 
-const getters = {}
+const getters = {
+  getProjects: state => state._projects
+}
 
 export default {
   namespaced: true,

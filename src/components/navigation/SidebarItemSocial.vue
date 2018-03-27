3d1249e66c3015030c345a04b1f7400fc0f77e38
Ben Garcia <ben@seebenprogram.com>
2018-03-27 10:11:14 -0400

    Build component for external navigation links

---
+++ src/components/navigation/SidebarItemSocial.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="align-items-center">
+    <li
+      :class="liClasses"
+    >
+      <a
+        :class="sidebarItemsClasses.a"
+        :style="sidebarItemsStyles"
+        :href="href"
+        target="_blank"
+      >
+        <i
+          class="material-icons align-middle"
+        >
+          {{ icon }}
+        </i>
+        <span
+          :href="href"
+          :class="sidebarItemsClasses.span"
+          v-html="name"
+        >
+        </span>
+      </a>
+    </li>
+  </div>
+</template>
+
+<script>
+export default {
+  props: [
+    'href',
+    'name',
+    'icon',
+    'liClasses',
+    'sidebarItemsClasses',
+    'sidebarItemsStyles'
+  ]
+}
+</script>
+
+<style lang="scss" scoped>
+a:hover {
+  background-color: #ddd;
+}
+</style>

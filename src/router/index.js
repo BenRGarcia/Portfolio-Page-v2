9abdc03fe1a0b60f89b9d472f5207b600e50e7a2
Ben Garcia <ben@seebenprogram.com>
2018-03-27 11:08:08 -0400

    Remove unused path

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,6 @@ import Router from 'vue-router'
 import Home from '@/components/Home'
 import Portfolio from '@/components/Portfolio'
 import Resume from '@/components/Resume'
-import Contact from '@/components/Contact'
 
 Vue.use(Router)
 
@@ -23,11 +22,6 @@ export default new Router({
       path: '/resume',
       name: 'Resume',
       component: Resume
-    },
-    {
-      path: '/contact',
-      name: 'Contact',
-      component: Contact
     },
     {
       path: '*',

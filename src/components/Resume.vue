1e2053dd86d845b4fb8ace7d1fd1535cc042082c
Ben Garcia <ben@seebenprogram.com>
2018-03-29 09:24:51 -0400

    embed iframe of google doc resume

--- src/components/Resume.vue
+++ src/components/Resume.vue
@@ -1,9 +1,16 @@
 <template>
-  <p>Resume Test</p>
+  <iframe
+    :src="googleDocSrc">
+  </iframe>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      googleDocSrc: 'https://docs.google.com/document/d/e/2PACX-1vRUxGTi7SuTagw9LYU-b0cgNl8EYOjAR7OHGDMxlTDTLwk89xtwhuzNJDw65ByMSySqjE1cIhfUVwPH/pub?embedded=true'
+    }
+  },
   mounted: function () {
     return this.$store.dispatch('collapseNavbar')
   }
@@ -11,5 +18,12 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+iframe {
+  width: 100%;
+  height: 80vh;
+  border-width: 2px;
+  border-style: inset;
+  border-color: initial;
+  border-image: initial;
+}
 </style>

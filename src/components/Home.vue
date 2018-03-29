51d98a7397fe2c78a86454540fded5835965ca13
Ben Garcia <ben@seebenprogram.com>
2018-03-29 17:49:42 -0400

    Import dev tools component

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,15 +1,75 @@
 <template>
   <div>
-    <Jumbotron/>
+    <div class="row">
+      <!-- Jumbotron -->
+      <div class="col-12">
+        <Jumbotron/>
+      </div>
+      <!-- Container -->
+      <div class="container">
+        <!-- Card container for dev tools -->
+        <div class="card" style="border-radius: 20px!important;">
+          <div class="card-header pl-4">
+            <h2 class="mb-0">Skills</h2>
+          </div>
+          <div class="card-body">
+            <div class="row">
+              <!-- Images of Tools -->
+              <DevTools
+                v-for="(tool, id) in tools"
+                :key="id"
+                :tool="tool"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import Jumbotron from './jumbotron/Jumbotron'
+import DevTools from './devtools/DevTools'
 
 export default {
   components: {
-    Jumbotron
+    Jumbotron,
+    DevTools
+  },
+  data () {
+    return {
+      tools: [
+        {
+          name: 'JavaScript',
+          src: require('../assets/images/tools/javascript.png')
+        }, {
+          name: 'Vue.js',
+          src: require('../assets/images/tools/vue.svg')
+        }, {
+          name: 'React.js',
+          src: require('../assets/images/tools/react.svg')
+        }, {
+          name: 'Node.js',
+          src: require('../assets/images/tools/node.svg')
+        }, {
+          name: 'Express.js',
+          src: require('../assets/images/tools/express.png')
+        }, {
+          name: 'MongoDB',
+          src: require('../assets/images/tools/mongo.svg')
+        }, {
+          name: 'Git',
+          src: require('../assets/images/tools/git.svg')
+        }, {
+          name: 'HTML5',
+          src: require('../assets/images/tools/html5.svg')
+        }, {
+          name: 'CSS3',
+          src: require('../assets/images/tools/css3.svg')
+        }
+      ]
+    }
   },
   mounted: function () {
     return this.$store.dispatch('collapseNavbar')
@@ -18,5 +78,9 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+.card {
+  border: 2px solid #ff0000;
+  box-shadow: 4px 4px 10px 0 rgba(0, 0, 0, 0.15),
+              6px 8px 18px 0 rgba(0, 0, 0, 0.1);
+}
 </style>
---
+++ src/components/devtools/DevTools.vue
@@ -0,0 +1,55 @@
+<template>
+  <div
+    :class="css.class.grid"
+  >
+    <div class="card m-2">
+      <div class="card-header text-center">
+        <h5 class="mb-0">{{ tool.name }}</h5>
+      </div>
+      <div class="card-body pb-1">
+        <div id="imgWrapper" class="text-center">
+          <img
+            :src="tool.src"
+            :alt="tool.name"
+            :class="css.class.img"
+          >
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: [
+    'tool'
+  ],
+  data () {
+    return {
+      css: {
+        class: {
+          grid: [
+            'col-12',
+            'col-sm-6',
+            'col-md-4'
+          ],
+          img: [
+            'img-fluid',
+            'mb-2'
+          ]
+        }
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+img {
+  height: 70px;
+}
+.card {
+  box-shadow: 0 2px  5px 0 rgba(0, 0, 0, 0.1),
+              0 4px 10px 0 rgba(0, 0, 0, 0.08);
+}
+</style>

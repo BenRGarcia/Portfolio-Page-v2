a48d30f96cdc7c0476a608cb883617c765fadbad
Ben Garcia <ben@seebenprogram.com>
2018-03-29 21:59:56 -0400

    Build toggl data component

---
+++ src/store/modules/toggl.js
@@ -0,0 +1,21 @@
+const state = {
+  _months: [
+    {
+      month: 'January',
+      src: require('../assets/toggl/2018-01_1134x533.png')
+    }, {
+      month: 'February',
+      src: require('../assets/toggl/2018-02_1134x533.png')
+    }
+  ]
+}
+
+const getters = {
+  getMonths: state => state._months
+}
+
+export default {
+  namespaced: true,
+  state,
+  getters
+}

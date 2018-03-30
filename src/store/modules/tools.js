f7c847804bfbde5b823f37b13747592e2359f267
Ben Garcia <ben@seebenprogram.com>
2018-03-29 21:57:54 -0400

    Build dev tools module

--- src/store/modules/tools.js
+++ src/store/modules/tools.js
@@ -1,9 +1,38 @@
 const state = {
-
+  _tools: [
+    {
+      name: 'JavaScript',
+      src: require('../../assets/images/tools/javascript.png')
+    }, {
+      name: 'Vue.js',
+      src: require('../../assets/images/tools/vue.svg')
+    }, {
+      name: 'React.js',
+      src: require('../../assets/images/tools/react.svg')
+    }, {
+      name: 'Node.js',
+      src: require('../../assets/images/tools/node.svg')
+    }, {
+      name: 'Express.js',
+      src: require('../../assets/images/tools/express.png')
+    }, {
+      name: 'MongoDB',
+      src: require('../../assets/images/tools/mongo.svg')
+    }, {
+      name: 'Git',
+      src: require('../../assets/images/tools/git.svg')
+    }, {
+      name: 'HTML5',
+      src: require('../../assets/images/tools/html5.svg')
+    }, {
+      name: 'CSS3',
+      src: require('../../assets/images/tools/css3.svg')
+    }
+  ]
 }
 
 const getters = {
-
+  getTools: state => state._tools
 }
 
 export default {

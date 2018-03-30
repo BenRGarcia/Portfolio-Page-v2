04d0dea97549f2fc9f4e9f787148165d39afe688
Ben Garcia <ben@seebenprogram.com>
2018-03-29 23:09:49 -0400

    Build card component, import carousel

---
+++ src/components/dedication/DedicationCard.vue
@@ -0,0 +1,46 @@
+<template>
+  <!-- Card container for dev tools -->
+  <div
+    class="card"
+  >
+    <div
+      class="card-header pl-4"
+    >
+      <h2
+        class="mb-0"
+      >
+        {{ cardTitle }}
+      </h2>
+    </div>
+    <div
+      class="card-body"
+    >
+      <div
+        class="row"
+      >
+        <Carousel/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Carousel from '../carousel/Carousel'
+
+export default {
+  data () {
+    return {
+      cardTitle: 'Dedication'
+    }
+  },
+  components: {
+    Carousel
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.card {
+  border-radius: 20px;
+}
+</style>

b79c776414231fc3c11011c7f0b29f027e89fdbe
Ben Garcia <ben@seebenprogram.com>
2018-03-29 20:03:22 -0400

    Add .pdf loading config

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -75,6 +75,14 @@ module.exports = {
           limit: 10000,
           name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
         }
+      },
+      {
+        test: /\.(pdf)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: utils.assetsPath('docs/[name].[hash:7].[ext]')
+        }
       }
     ]
   },

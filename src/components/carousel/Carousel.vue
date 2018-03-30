b2dcd1a0f9a8f7ac078970eeb241d78178f96a30
Ben Garcia <ben@seebenprogram.com>
2018-03-29 22:48:53 -0400

    Initial commit, add computed getter method

---
+++ src/components/carousel/Carousel.vue
@@ -0,0 +1,25 @@
+<template>
+
+</template>
+
+<script>
+import Month from './Toggl'
+
+export default {
+  data () {
+    return {
+      // stuff
+    }
+  },
+  components: {
+    Month
+  },
+  computed: {
+    getMonths: () => this.$store.getters['toggl/getMonths']
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>

5ba92a7ae3425ba5691c2eaead17f2e43ab9ff71
Ben Garcia <ben@seebenprogram.com>
2018-03-29 22:49:14 -0400

    Initial commit

--- src/components/carousel/Toggl.vue
+++ src/components/carousel/Toggl.vue
@@ -1,9 +1,22 @@
 <template>
-  
+    <!-- Image Carousel -->
+    <div class="carousel-item">
+      <img
+        :src="month.src"
+        :alt="month.name"
+      >
+      <div class="carousel-caption d-none d-md-block">
+        <h5>{{ month.name }}</h5>
+        <p>Monthly Hours of Coding</p>
+      </div>
+    </div>
 </template>
 
 <script>
 export default {
+  props: [
+    'month'
+  ],
   data () {
     return {
       // ... stuff

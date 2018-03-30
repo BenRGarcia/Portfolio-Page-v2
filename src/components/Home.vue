3442d8f93fae4f34b60b05b71850966dc60c5b9e
Ben Garcia <ben@seebenprogram.com>
2018-03-29 22:41:24 -0400

    Remove craziness to separate component

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -7,36 +7,12 @@
       </div>
       <!-- Container for 'Skills' -->
       <div class="container">
-        <!-- Card container for dev tools -->
-        <div class="card" style="border-radius: 20px!important;">
-          <div class="card-header pl-4">
-            <h2 class="mb-0">Skills</h2>
-          </div>
-          <div class="card-body">
-            <div class="row">
-              <!-- Images of Tools -->
-              <DevTools
-                v-for="(tool, id) in tools"
-                :key="id"
-                :tool="tool"
-              />
-            </div>
-          </div>
-        </div>
+        <!-- Images of Tools -->
+        <SkillsCard/>
       </div>
       <!-- Container for 'Dedication' -->
       <div class="container">
-        <!-- Card container for dev tools -->
-        <div class="card" style="border-radius: 20px!important;">
-          <div class="card-header pl-4">
-            <h2 class="mb-0">Dedication</h2>
-          </div>
-          <div class="card-body">
-            <div class="row">
-              <!-- Chart of Hours Studied by Month -->
-            </div>
-          </div>
-        </div>
+        <!-- Carousel goes here -->
       </div>
     </div>
   </div>
@@ -44,46 +20,20 @@
 
 <script>
 import Jumbotron from './jumbotron/Jumbotron'
-import DevTools from './devtools/DevTools'
+import SkillsCard from './skills/SkillsCard'
 
 export default {
   components: {
     Jumbotron,
-    DevTools
+    SkillsCard
   },
   data () {
     return {
-      tools: [
-        {
-          name: 'JavaScript',
-          src: require('../assets/images/tools/javascript.png')
-        }, {
-          name: 'Vue.js',
-          src: require('../assets/images/tools/vue.svg')
-        }, {
-          name: 'React.js',
-          src: require('../assets/images/tools/react.svg')
-        }, {
-          name: 'Node.js',
-          src: require('../assets/images/tools/node.svg')
-        }, {
-          name: 'Express.js',
-          src: require('../assets/images/tools/express.png')
-        }, {
-          name: 'MongoDB',
-          src: require('../assets/images/tools/mongo.svg')
-        }, {
-          name: 'Git',
-          src: require('../assets/images/tools/git.svg')
-        }, {
-          name: 'HTML5',
-          src: require('../assets/images/tools/html5.svg')
-        }, {
-          name: 'CSS3',
-          src: require('../assets/images/tools/css3.svg')
-        }
-      ]
+      // stuff
     }
+  },
+  computed: {
+    getTools: () => this.$store.getters['tools/getTools']
   },
   mounted: function () {
     return this.$store.dispatch('collapseNavbar')
@@ -96,6 +46,6 @@ export default {
   margin-bottom: 15px;
   border: 2px solid #ff0000;
   box-shadow: 4px 4px 10px 0 rgba(0, 0, 0, 0.15),
-              6px 8px 18px 0 rgba(0, 0, 0, 0.1);
+              6px 8px 18px 0 rgba(0, 0, 0, 0.10);
 }
 </style>

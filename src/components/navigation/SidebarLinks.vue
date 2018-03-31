f835b31117de4f3b2a3a554ac8a7f7fba2befec3
Ben Garcia <ben@seebenprogram.com>
2018-03-31 13:59:14 -0400

    Refactor to handle both internal and external links

---
+++ src/components/navigation/SidebarLinks.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="align-items-center">
+    <!-- Render if 'RouterLink' property is present (Internal link) -->
+    <router-link
+      v-if="link.routerLink"
+      tag="li"
+      :class="css.class.li"
+      :to="{ name: link.routerLink}"
+    >
+      <a
+        :class="css.class.a"
+        :style="css.style.a"
+      >
+        <i
+          class="material-icons align-middle"
+        >
+          {{ link.icon }}
+        </i>
+        <span
+          :class="css.class.span"
+          :href="link.href"
+          v-html="link.name"
+        >
+        </span>
+      </a>
+    </router-link>
+    <!-- Render if 'RouterLink' property is present (External link) -->
+    <li
+      v-else
+      :class="css.class.li"
+    >
+      <a
+        :class="css.class.a"
+        :style="css.style.a"
+        :href="link.href"
+        :target="link.target"
+      >
+        <i
+          class="material-icons align-middle"
+        >
+          {{ link.icon }}
+        </i>
+        <span
+          :class="css.class.span"
+          :href="link.href"
+          v-html="link.name"
+        >
+        </span>
+      </a>
+    </li>
+  </div>
+</template>
+
+<script>
+export default {
+  props: [
+    'link',
+    'css'
+  ]
+}
+</script>
+
+<style lang="scss" scoped>
+a:hover {
+  background-color: #ddd;
+}
+</style>

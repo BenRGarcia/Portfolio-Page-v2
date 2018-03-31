580685daf7f599a533e964125403d72962c9f0d4
Ben Garcia <ben@seebenprogram.com>
2018-03-31 09:26:58 -0400

    abandon idea

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -26,15 +26,24 @@
     <div
       class="container"
     >
-      <!-- Bootstrap Carousel -->
-      <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-        <!-- Hard Coded Carousel Images :-( -->
-        <div class="carousel-inner">
-          <div class="carousel-item active">
-            <img class="d-block w-100" :src="months.January" alt="First slide">
-          </div>
-          <div class="carousel-item">
-            <img class="d-block w-100" :src="months.February" alt="Second slide">
+      <div class="card">
+        <div class="card-header pl-4">
+          <h2 class="mb-0">
+            Dedication
+          </h2>
+        </div>
+        <div class="card-body">
+          <!-- Bootstrap Carousel -->
+          <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+            <!-- Hard Coded Carousel Images :-( -->
+            <div class="carousel-inner">
+              <div class="carousel-item active">
+                <img class="d-block w-100" :src="months.January" alt="First slide">
+              </div>
+              <div class="carousel-item">
+                <img class="d-block w-100" :src="months.February" alt="Second slide">
+              </div>
+            </div>
           </div>
         </div>
       </div>
@@ -77,11 +86,12 @@ export default {
 .card {
   margin-bottom: 15px;
   border: 2px solid #ff0000;
+  border-radius: 20px;
   box-shadow: 4px 4px 10px 0 rgba(0, 0, 0, 0.15),
               6px 8px 18px 0 rgba(0, 0, 0, 0.10);
 }
 #carouselExampleIndicators {
-  box-shadow: 4px 4px 20px 0 rgba(0, 0, 0, 0.15),
-              6px 8px 18px 0 rgba(0, 0, 0, 0.30);
+  box-shadow: 2px 2px 10px 0 rgba(0, 0, 0, 0.07),
+              3px 4px 9px 0 rgba(0, 0, 0, 0.25);
 }
 </style>
